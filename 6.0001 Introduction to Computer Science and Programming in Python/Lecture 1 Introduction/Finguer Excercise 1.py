e10f294bac3649b0d7d7c5e8f54a7dcdd682d2db
jdrangosch <30609410+jdrangosch@users.noreply.github.com>
2024-05-30 20:36:10 -0300

    Add local files to repository.
    Add local files

---
+++ 6.0001 Introduction to Computer Science and Programming in Python/Lecture 1 Introduction/Finguer Excercise 1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 30 21:01:09 2024
+
+@author: juls
+"""
+
+# Assume 3 variables are already defined for you: a, b, and c. 
+# Create a variable called total that adds a and b then multiplies the result by c. 
+# Include a last line in your code to print the value: print(total)
+
+a = 1
+b = 2
+c = 3 
+total = (a+b)*c
+print(total)
---
+++ 6.0001 Introduction to Computer Science and Programming in Python/Lecture 1 Introduction/ps0.py
@@ -0,0 +1,13 @@
+# Problem Set 0
+# Name: Julian Drangosch
+# Collaborator: ---
+# Time Spent: 1 min
+
+# ---------- SEPARATOR -------------
+
+import numpy
+x = 5
+y = 8
+z = x + y
+a = numpy.log2(z)
+print(a)
